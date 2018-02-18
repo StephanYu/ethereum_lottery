805e373fb3fef76148686d24f415ca958657e650
Stephan Yu <Stephan.yu@gmail.com>
2018-02-18 07:56:05 +0000

    Add dotenv for env variables

--- deploy.js
+++ deploy.js
@@ -1,8 +1,9 @@
+require('dotenv').load();
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 const provider = new HDWalletProvider(
-  `${process.env.METAMASK_SECRET}`,
+  `${process.env.MNEMONIC}`,
   `https://rinkeby.infura.io/${process.env.INFURA_API_KEY}`
 );
 const web3 = new Web3(provider);
@@ -16,7 +17,7 @@ const deploy = async () => {
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
     .send({ gas: '1000000', from: account });
-
+  console.log('interface', interface);
   console.log('Contract succesfully deployed to', result.options.address);
 };
 
